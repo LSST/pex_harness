721683bbc291977028225440c22b4b3018fd9ec7
daues <daues@git.lsstcorp.org>
2007-12-04 18:12:42 +0000

    merge of 105 adds an example

---
+++ examples/inputoutput/triggerIpdpEventTransmit.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import lsst.mwi.data as datap
+import lsst.events as events
+import time
+
+if __name__ == "__main__":
+    print "starting...\n"
+    externalEventTransmitter = events.EventTransmitter("lsst8.ncsa.uiuc.edu", "triggerIpdpEvent")
+
+    root = datap.SupportFactory.createPropertyNode("root");
+
+    VISITID = datap.DataProperty("visitid", "fov391")
+
+    root.addProperty(VISITID)
+
+    externalEventTransmitter.publish("eventtype", root)
+
