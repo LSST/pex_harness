642c06157d0e4d167c81903fa416a0f2bb4c5080
ktlim <ktlim@git.lsstcorp.org>
2007-12-14 17:21:31 +0000

    Add stage that creates symlinks based on pathname patterns in Policy and data on clipboard.

---
+++ python/lsst/dps/SymLinkStage.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- python -*-
+
+"""
+SymLinkStage provides a generic mechanism for symbolically linking files or
+directories.
+"""
+
+import lsst.dps.Stage
+import lsst.mwi.utils
+import os
+from lsst.mwi.logging import Log
+
+def getDPValue(dp):
+    try:
+        value = dp.getValueString()
+        return value
+    except:
+        pass
+    try:
+        value = dp.getValueInt64()
+        return value
+    except:
+        pass
+    try:
+        value = dp.getValueInt()
+        return value
+    except:
+        pass
+    try:
+        value = dp.getValueFloat()
+        return value
+    except:
+        pass
+    try:
+        value = dp.getValueBool()
+        return value
+    except:
+        pass
+    raise Runtime, 'Unknown DataProperty value type in SymLinkStage'
+
+def createAdditionalData(stage, stagePolicy, clipboard):
+    """
+    Extract additionalData values, as specified by policy, from the clipboard.
+    """
+
+    additionalData = {}
+
+    # Parse array of "key=clipboard-key" or
+    # "key=clipboard-key.dataproperty-key" mappings
+    if stagePolicy.exists('AdditionalData'):
+        dataPairs = stagePolicy.getStringArray('AdditionalData')
+        for pair in dataPairs:
+            (rename, name) = pair.split("=")
+            if name.find(".") != -1:
+                (clipKey, dpKey) = name.split(".", 1)
+                dp = clipboard.get(clipKey).findUnique(dpKey)
+                value = getDPValue(dp)
+            else:
+                value = clipboard.get(name)
+            additionalData[rename] = value
+            lsst.mwi.utils.Trace("SymLinkStage", 3, "AdditionalData item: " + pair)
+
+    # Add the predefined runId, sliceId, and universeSize keys
+
+    additionalData['runId'] = stage.getRun()
+    additionalData['sliceId'] = stage.getRank()
+    additionalData['ccdId'] = "%02d" % (stage.getRank() + 1)
+    additionalData['universeSize'] = stage.getUniverseSize()
+
+    lsst.mwi.utils.Trace("SymLinkStage", 3, "additionalData:\n" + \
+        str(additionalData))
+
+    return additionalData
+
+class SymLinkStage (lsst.dps.Stage.Stage):
+    """
+    A Stage that symlinks files or directories.
+    """
+
+    def __init__(self, stageId, stagePolicy):
+        """
+        Initialize the stage with a policy.
+        """
+
+        lsst.dps.Stage.Stage.__init__(self, stageId, stagePolicy)
+
+    def preprocess(self):
+        """
+        Persist the requested data in the master process before any
+        (subclass) processing, if desired.
+        """
+        self.activeClipboard = self.inputQueue.getNextDataset()
+        if not self._policy.exists('Links'):
+            mylog = Log(Log.defaultLog(), "SymLinkStage")
+            mylog.log(Log.WARN, "No Links found")
+            return
+
+        additionalData = createAdditionalData(self, self._policy, \
+                self.activeClipboard)
+
+        linkPolicyList = self._policy.getPolicyArray('Links')
+        for linkPolicy in linkPolicyList:
+            sourcePath = linkPolicy.getString('sourcePath') % additionalData
+            destPath = linkPolicy.getString('destPath') % additionalData
+            lsst.mwi.utils.Trace("SymLinkStage", 3, "linking %s to %s" % \
+                    (sourcePath, destPath))
+            os.symlink(sourcePath, destPath)
---
+++ test/lsst/dps/test/TestSymLink.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import lsst.dps.SymLinkStage
+import lsst.dps.Clipboard
+import lsst.dps.Queue
+import lsst.mwi.data
+import lsst.mwi.policy
+
+# Get the policies
+linkPolicy = lsst.mwi.policy.Policy.createPolicy("test/policy/symlink.policy")
+
+# Create the queues
+q1 = lsst.dps.Queue.Queue()
+q2 = lsst.dps.Queue.Queue()
+
+# Create and initialize the stage
+linkStage = lsst.dps.SymLinkStage.SymLinkStage(1, linkPolicy)
+linkStage.initialize(q2, q1)
+linkStage.setUniverseSize(100)
+linkStage.setRank(42)
+linkStage.setRun("TEST")
+
+# Create the event DataProperty
+event = lsst.mwi.data.SupportFactory.createPropertyNode("root")
+child = lsst.mwi.data.DataProperty("visitId", 3840391)
+event.addProperty(child)
+child = lsst.mwi.data.DataProperty("exposureId", 3840391)
+event.addProperty(child)
+child = lsst.mwi.data.DataProperty("filterName", "i")
+event.addProperty(child)
+
+# Create the clipboard and put the event on it
+clip = lsst.dps.Clipboard.Clipboard()
+clip.put("triggerVisitEvent", event)
+
+# Put the clipboard on the input queue
+q1.addDataset(clip)
+
+# Run the link stage like the master
+linkStage.preprocess()
+linkStage.postprocess()
+
+# Check the output queue: should have everything we put on the input
+assert q2.size() == 1
+clip2 = q2.getNextDataset()
+assert clip2
+assert clip2.get("triggerVisitEvent")
