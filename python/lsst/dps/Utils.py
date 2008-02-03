5349e8bcba09bdf4a7be99ce8173c6cfa647467e
ktlim <ktlim@git.lsstcorp.org>
2008-02-03 02:13:20 +0000

    Have ccdIds using CcdOffset start with 1 instead of 0.

--- python/lsst/dps/Utils.py
+++ python/lsst/dps/Utils.py
@@ -84,11 +84,8 @@ def createAdditionalData(stage, stagePolicy, clipboard):
         formula = re.sub(r'@slice', r'stage.getRank()', formula)
         ccdId = eval(formula)
     else:
-        if stagePolicy.exists('CcdOffset'):
-            incr = stagePolicy.get('CcdOffset')
-        else:
-            incr = 1
-        ccdId = "%03d" % (stage.getRank() + incr)
+        incr = stagePolicy.get('CcdOffset', 0)
+        ccdId = "%03d" % (stage.getRank() + 1 + incr)
     leaf = lsst.mwi.data.DataProperty('ccdId', ccdId)
     dataProperty.addProperty(leaf)
 
