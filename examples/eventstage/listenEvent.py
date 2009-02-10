277b957a4ef85c780a5dad429ae49a3421b6729a
daues <daues@git.lsstcorp.org>
2009-02-10 02:42:22 +0000

    update eventstage example

--- examples/eventstage/listenEvent.py
+++ examples/eventstage/listenEvent.py
@@ -10,10 +10,17 @@ import time
 if __name__ == "__main__":
     print "starting...\n"
 
-    activemqBroker = "lsst8.ncsa.uiuc.edu"
+    eventBrokerHost = "lsst8.ncsa.uiuc.edu"
 
-    eventRec = events.EventReceiver(activemqBroker, "outgoingEvent")
+    eventRec = events.EventReceiver(eventBrokerHost, "outgoingEvent")
 
     print 'Python listenEvents : waiting on receive...\n'
     inputParamPropertySetPtr = eventRec.receive(800000)
+
     print 'Python listenEvents : - received event.\n'
+
+    nameList = inputParamPropertySetPtr.names()
+ 
+    for name in nameList:
+        print 'name is ' + name
+
