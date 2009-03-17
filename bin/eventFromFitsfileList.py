6cef81e249f36649efc3fbf5003bb75cdbc73af1
fpierfed <fpierfed@git.lsstcorp.org>
2009-03-17 18:08:33 +0000

    Added more realistic sleep times. Fixed the 'send too many events' problem.

--- bin/eventFromFitsfileList.py
+++ bin/eventFromFitsfileList.py
@@ -13,29 +13,46 @@ import lsst.ctrl.events as ctrlEvents
 from lsst.ctrl.dc3pipe.MetadataStages import transformMetadata, validateMetadata
 
 import lsst.pex.logging as logging
+
+# Import EventFromInputfile from eventFromFitsfile.py so that we do not have to
+# re-write that one.
+
+
+# Constants
+EXP_TIME = 15.
+SLEW_TIME = 5.
+
+
+
+
+# Verbosity/logging.
 Verbosity = 4
 logging.Trace_setVerbosity('dc3pipe', Verbosity)
 
 
 
-
-
 def EventFromInputFileList(inputfile, 
                            datatypePolicy, 
-                           sleepTime=None,
+                           expTime=15,
+                           slweTime=5,
                            rootTopicName='triggerImageprocEvent', 
                            hostName='lsst8.ncsa.uiuc.edu'):
     """
     Generate events for the IPSD (and MOPS) pipeline by reading a list of visit
     directories and extracting the relevant information from the FITS files 
     therein.
-    
-    The script sends events every <sleep time> seconds or, if that is not 
-    specified, every <exposure time> seconds (extracted from each FITS file).
-    
+
+    The two optional parameters are the exposure time of each exposure in a 
+    visit (assumed constant) and the average slew time of the telescope. Both 
+    are in seconds. The default to 15 seconds and 5 seconds respecively.
+
+    The script sends one event for the first visit exposure, waits <exp time> 
+    sec and then sends an event for the second exposure. At that point, it waits
+    (<exp time> + <slew time>) sec before passing to the next visit.
+
     The input directory list is a simple text file listing visit directories one
     per line. Comments start with a '#' and are ignored. It is assumed that the 
-    name of each directory in the file is a valid visitId. Also it is assumed
+    name of each directory in the file is a valid visitId. Also it is assumed 
     that each directory has the following structure:
         visitId/
                 0/
@@ -45,6 +62,10 @@ def EventFromInputFileList(inputfile,
     
     @param inputfile: name of the directory list file.
     @param datatypePolicy: Policy file for the input data.
+    @param expTime: assume each exposure in a visit is expTime long (defualts to
+           15 seconds for DC3). expTime is in seconds.
+    @param slewTime: assume an average slew time of slewTime seconds (defualts 
+           to 5 seconds for DC3). slewTime is in seconds.
     @param rootTopicName: root name for the event's topic. The final topic will 
            be rootTopicName+'0' or rootTopicName+'1' depending on whether the
            event refers to the first or second image of the visit.
@@ -59,7 +80,7 @@ def EventFromInputFileList(inputfile,
     
     # Covenience function.
     def sendEvent(f):
-        return(EventFromInputfile(f, datatypePolicy, metadataPolicy, sleepTime,
+        return(EventFromInputfile(f, datatypePolicy, metadataPolicy,
                                   rootTopicName, hostName))
     
     f = open(inputfile)
@@ -68,9 +89,11 @@ def EventFromInputFileList(inputfile,
         if(line.startswith('#')):
             continue
         
+        # Get the list of amp FITS files in each dir.
         fileList0 = glob.glob(os.path.join(dirName, '0', '*.fits'))
         fileList1 = glob.glob(os.path.join(dirName, '1', '*.fits'))
         
+        # Simple sanity check.
         if(len(fileList0) != len(fileList1)):
             pexLog.Trace('dc3pipe.eventfrominputfilelist', 1, 
                          'Skipping %s: wrong file count in 0 and 1' \
@@ -78,10 +101,16 @@ def EventFromInputFileList(inputfile,
             continue
         
         # Now we just trust that the i-th file in 0 corresponds to the i-th file
-        # in 1...
-        for i in range(len(fileList0)):
-            sendEvent(fileList0[i])
-            sendEvent(fileList1[i])
+        # in 1... Fortunately, we only need to send one event per image 
+        # directory, since all images there are one MEF split into individual 
+        # amps.
+        sendEvent(fileList0[0])
+        # Sleep some.
+        time.sleep(expTime)
+        # Next event.
+        sendEvent(fileList1[0])
+        # Sleep expTime + slewTime.
+        time.sleep(expTime + slewTime)
     f.close()
     return
     
@@ -90,7 +119,6 @@ def EventFromInputFileList(inputfile,
 def EventFromInputfile(inputfile, 
                        datatypePolicy, 
                        metadataPolicy,
-                       sleepTime=None,
                        rootTopicName='triggerImageprocEvent', 
                        hostName='lsst8.ncsa.uiuc.edu'):
     # For DC3a, inputfile is a .fits file on disk
@@ -126,25 +154,25 @@ def EventFromInputfile(inputfile,
     elif event.getInt('exposureId') == 1:
         eventTransmitter = ctrlEvents.EventTransmitter(hostName, topicName+'1')
 
-    # eventTransmitter.publish(event)
-    print('Sending event for file %s' %(inputfile))
-    if(sleepTime == None):
-        print('Sleeping for %f seconds' %(event.get('expTime')))
-        time.sleep(float(event.get('expTime')))
-    else:
-        time.sleep(sleepTime)
+    eventTransmitter.publish(event)
+    # print('Sending event for file %s' %(inputfile))
     return True
 
 
 if __name__ == "__main__":
     USAGE = '''
-usage: eventFromFitsFileList.py <dir_list_file> <policy_file> [<sleep time>]
+usage: eventFromFitsFileList.py <dir_list_file> <policy_file> [<exp time>] [<slew time>]
     
 Generate events for the IPSD (and MOPS) pipeline by reading a list of visit
 directories and extracting the relevant information from the FITS files therein.
 
-The script sends events every <sleep time> seconds or, if that is not specified,
-every <exposure time> seconds (extracted from each FITS file).
+The two optional parameters are the exposure time of each exposure in a visit
+(assumed constant) and the average slew time of the telescope. Both are in 
+seconds. The default to 15 seconds and 5 seconds respecively.
+
+The script sends one event for the first visit exposure, waits <exp time> sec 
+and then sends an event for the second exposure. At that point, it waits 
+(<exp time> + <slew time>) sec before passing to the next visit.
 
 The input directory list is a simple text file listing visit directories one per
 line. Comments start with a '#' and are ignored. It is assumed that the name of 
@@ -158,19 +186,24 @@ directory has the following structure:
 '''
     
     
-    if(len(sys.argv) not in (3, 4)):
+    if(len(sys.argv) not in (3, 4, 5)):
         sys.stderr.write(USAGE)
         sys.exit(1)
     
     inputDirectoryList = sys.argv[1]
     datatypePolicy = pexPolicy.Policy.createPolicy(sys.argv[2])
-    sleepTime = None
+    expTime = EXP_TIME
+    slewTime = SLEW_TIME
     if(len(sys.argv) == 4):
         try:
-            sleepTime = int(sys.argv[4])
+            expTime = int(sys.argv[4])
         except:
-            sys.stderr.write(USAGE)
-            sys.exit(2)
+            pass
+    if(len(sys.argv) == 5):
+        try:
+            slewTime = int(sys.argv[4])
+        except:
+            pass
     
     # Extract broker info etc.
     pipelinePolicy = dafBase.PropertySet()
@@ -183,6 +216,6 @@ directory has the following structure:
     else:
         topicName = 'triggerImageprocEvent'
     
-    EventFromInputFileList(inputDirectoryList, datatypePolicy, sleepTime,
-                           topicName, hostName)
+    EventFromInputFileList(inputDirectoryList, datatypePolicy, expTime, 
+                           slewTime, topicName, hostName)
         
