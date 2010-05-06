d511f180b97bd691cbf837cfdcb47eabbfbd3d80
daues <daues@git.lsstcorp.org>
2010-05-06 20:44:38 +0000

    update shutdown example to use Event object

--- examples/shutdowntest/triggerShutdownEventTransmitter.py
+++ examples/shutdowntest/triggerShutdownEventTransmitter.py
@@ -13,14 +13,21 @@ if __name__ == "__main__":
     shutdownTopic = "triggerShutdownA"
     eventBrokerHost = "lsst8.ncsa.uiuc.edu"
 
-    externalEventTransmitter = events.EventTransmitter(eventBrokerHost, shutdownTopic )
+    externalEventTransmitter = events.EventTransmitter(eventBrokerHost, shutdownTopic)
 
     root = PropertySet()
+    root.set("STATUS", "some status message")
+    root.set("TOPIC", shutdownTopic)
+
     # Shutdown at level 1 : stop immediately by killing process (ugly)
     # Shutdown at level 2 : exit in a clean manner (Pipeline and Slices) at a synchronization point 
     # Shutdown at level 3 : exit in a clean manner (Pipeline and Slices) at the end of a Stage
     # Shutdown at level 4 : exit in a clean manner (Pipeline and Slices) at the end of a Visit
     root.setInt("level", 4)
 
-    externalEventTransmitter.publish(root)
+    eventSystem = events.EventSystem.getDefaultEventSystem();
+    originatorId = eventSystem.createOriginatorId()
+    event = events.StatusEvent("test_19001", originatorId, root)
+
+    externalEventTransmitter.publishEvent(event)
 
